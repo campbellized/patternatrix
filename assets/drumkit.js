b4643ca6ad4cc5578feff25a5369c2513df63639
campbellized <campbellized@gmail.com>
2016-12-23 17:03:12 -0500

    Refactored app into three seperate modules.

--- assets/drumkit.js
+++ assets/drumkit.js
@@ -1,78 +1,7 @@
-(function(jsDrum, window){
-    var module = {};
-    var tempo = 120;
-    var playing = false;
-    var recording = false;
-    var beat = 1;
-    var ticksPerSecond = tempo / 60;
-    var stepsPerSequence = 16;
-    var sequence = new Array(stepsPerSequence);
-
-    function incrementStep(){
-        if(playing){
-            document.querySelector("input[name='step']:nth-child("+beat+")").checked = true;
-            jsDrum.playSoundsInStep(beat);
-            // console.log(beat);
-            beat %= stepsPerSequence;
-            beat++;
-        }
-    }
-
-    module.tempo = function(){return tempo;};
-    module.isPlaying = function(){return playing;};
-    module.isRecording = function(){return recording;};
-    module.currBeat = function(){return beat;};
-    module.sequence = function(){return sequence;};
-    module.init = function initSequencer(){
-        console.log("YO");
-        for (var i = 0; i < stepsPerSequence; i++) {
-          sequence[i] = {};
-        }
-
-        setInterval(incrementStep, 1000 / ticksPerSecond);
-    }
-
-    console.log(module);
-
-    jsDrum.sequencer = module;
-
-    /* Add event listener to sequencer controls */
-    window.addEventListener("click", function(e){
-        if(e.srcElement.id === "play") {
-            playing = true;
-        }else if(e.srcElement.id === "stop") {
-            if(playing === false) {
-                document.querySelector("input[name='step']:nth-child(1)").checked = true;
-                beat = 1;
-            }
-            playing = false;
-        }else if(e.srcElement.id === "record"){
-            e.srcElement.classList.toggle("active");
-            recording = !recording;
-            console.log("Recording:", recording);
-        }else{
-            return;
-        }
-    });
-
-    /* Add event listener to sequencer step indicators */
-    window.addEventListener("click", function(e){
-        if(e.srcElement.name !== "step") return;
-        beat = e.srcElement.value;
-        deactivatePads();
-        activatePadsInStep(beat);
-    });
-})(window);
-
-var jsDrum = (function(){
-    'use strict';
-
+var jsDrum = (function(window){
     var local = true;
-    var sequencer = this.sequencer;
-    console.log(this);
-    console.log(this.sequencer);
-    console.log(sequencer);
-    sequencer.init();
+    var sequencer;
+    var ui;
 
     WebMidi.enable(function(err){
         if(err){
@@ -88,6 +17,14 @@ var jsDrum = (function(){
         });
     }
 
+    function intialize(){
+        sequencer = this.sequencer;
+        ui = this.ui;
+        // console.log("my sequencer", sequencer);
+        // console.log("my ui", ui);
+        sequencer.init();
+    }
+
     function playMIDI(note){
         if(WebMidi && WebMidi.outputs.length){
             output = WebMidi.outputs[0];
@@ -96,8 +33,8 @@ var jsDrum = (function(){
         if(local){
             playSound(note);
         }
-        if(sequencer.isRecording){
-            toggleNoteInStep(sequencer.currBeat, note);
+        if(sequencer.isRecording()){
+            sequencer.toggleNoteInStep(note);
         }
     }
 
@@ -105,60 +42,6 @@ var jsDrum = (function(){
         var audio = document.querySelector("audio[data-tone='"+note+"']");
         audio.currentTime = 0;
         audio.play();
-    }
-
-    function toggleNoteInStep(step, note){
-        if(sequencer.sequence[step - 1][note]){
-            delete sequencer.sequence[step - 1][note];
-        }else{
-            sequencer.sequence[step - 1][note] = 1;
-        }
-        togglePad(note);
-    }
-
-    function playSoundsInStep(step){
-        var sounds = Object.keys(sequencer.sequence[step - 1]);
-        var audio;
-        for(i = 0; i < sounds.length; i++){
-            playSound(sounds[i]);
-        }
-    }
-
-    function activatePad(note){
-        var p = document.querySelector(".pads__pad[data-note='"+note+"']");
-        p.classList.add("active");
-    }
-
-    function deactivatePad(note){
-        var p = document.querySelector(".pads__pad[data-note='"+note+"']");
-        p.classList.remove("active");
-    }
-
-    function togglePad(note){
-        var p = document.querySelector(".pads__pad[data-note='"+note+"']");
-        p.classList.toggle("active");
-    }
-
-    function activatePadsInStep(step){
-        var pads = Object.keys(sequencer.sequence[sequencer.currBeat - 1]);
-
-        for(i = 0; i < pads.length; i++){
-            activatePad(pads[i]);
-        }
-    }
-
-    function deactivatePads(){
-        var activePads = document.querySelectorAll(".pads__pad.active");
-
-        [].forEach.call(activePads, function(el) {
-            deactivatePad(el.getAttribute("data-note"));
-        });
-    }
-
-    return {
-        init: function(){
-
-        }
     }
 
     /* Capture Querty events and generate the appropriate MIDI note */
@@ -206,4 +89,138 @@ var jsDrum = (function(){
         }
         playMIDI(note);
     });
-})();
+
+    return {
+        init: intialize,
+        playNote: playSound
+    };
+})(window);
+
+/* Sequencer module */
+(function(jsDrum, window){
+    var sequencer = {};
+    var tempo = 120;
+    var playing = false;
+    var recording = false;
+    var beat = 1;
+    var ticksPerSecond = tempo / 60;
+    var stepsPerSequence = 16;
+    var sequence = new Array(stepsPerSequence);
+
+    function incrementStep(){
+        if(playing){
+            document.querySelector("input[name='step']:nth-child("+beat+")").checked = true;
+            playSoundsInStep(beat);
+            beat %= stepsPerSequence;
+            beat++;
+        }
+    }
+
+    function playSoundsInStep(step){
+        var sounds = Object.keys(sequence[step - 1]);
+
+        for(i = 0; i < sounds.length; i++){
+            jsDrum.playNote(sounds[i]);
+        }
+    }
+
+    function toggleNoteInStep(note){
+        if(sequence[beat - 1][note]){
+            delete sequence[beat - 1][note];
+        }else{
+            sequence[beat - 1][note] = 1;
+        }
+        jsDrum.ui.togglePad(note);
+    }
+
+    function initialize(){
+        for (var i = 0; i < stepsPerSequence; i++) {
+          sequence[i] = {};
+        }
+
+        setInterval(incrementStep, 1000 / ticksPerSecond);
+    }
+    sequencer.tempo = function(){return tempo;};
+    sequencer.isPlaying = function(){return playing;};
+    sequencer.isRecording = function(){return recording;};
+    sequencer.currBeat = function(){return beat;};
+    sequencer.sequence = function(){return sequence;};
+    sequencer.toggleNoteInStep = toggleNoteInStep;
+    sequencer.init = initialize;
+
+    /* Add event listener to sequencer controls */
+    window.addEventListener("click", function(e){
+        if(e.srcElement.id === "play") {
+            playing = true;
+        }else if(e.srcElement.id === "stop") {
+            if(playing === false) {
+                document.querySelector("input[name='step']:nth-child(1)").checked = true;
+                beat = 1;
+            }
+            playing = false;
+        }else if(e.srcElement.id === "record"){
+            e.srcElement.classList.toggle("active");
+            recording = !recording;
+        }else{
+            return;
+        }
+    });
+
+    /* Add event listener to sequencer step indicators */
+    window.addEventListener("click", function(e){
+        if(e.srcElement.name !== "step") return;
+        beat = e.srcElement.value;
+        jsDrum.ui.deactivatePads();
+        jsDrum.ui.activatePadsInStep(beat);
+    });
+
+    jsDrum.sequencer = sequencer;
+
+    return jsDrum;
+})(jsDrum, window);
+
+/* UI module */
+(function(jsDrum, document){
+    var ui = {};
+
+    function activatePad(note){
+        var p = document.querySelector(".pads__pad[data-note='"+note+"']");
+        p.classList.add("active");
+    }
+
+    function deactivatePad(note){
+        var p = document.querySelector(".pads__pad[data-note='"+note+"']");
+        p.classList.remove("active");
+    }
+
+    function togglePad(note){
+        var p = document.querySelector(".pads__pad[data-note='"+note+"']");
+        p.classList.toggle("active");
+    }
+
+    function activatePadsInStep(step){
+        var sequence = jsDrum.sequencer.sequence();
+        var pads = Object.keys(sequence[jsDrum.sequencer.currBeat() - 1]);
+
+        for(i = 0; i < pads.length; i++){
+            activatePad(pads[i]);
+        }
+    }
+
+    function deactivatePads(){
+        var activePads = document.querySelectorAll(".pads__pad.active");
+
+        [].forEach.call(activePads, function(el) {
+            deactivatePad(el.getAttribute("data-note"));
+        });
+    }
+
+    ui.togglePad = togglePad;
+    ui.activatePadsInStep = activatePadsInStep;
+    ui.deactivatePads = deactivatePads;
+    jsDrum.ui = ui;
+
+    return jsDrum;
+})(jsDrum, document);
+
+jsDrum.init();
